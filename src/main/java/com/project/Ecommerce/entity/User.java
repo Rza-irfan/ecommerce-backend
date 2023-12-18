165049d2bd968fc4685bff9e3263e38b52669eb5
ayazkhan <ayazkhan@403>
2023-12-18 22:54:08 +0530

    Initial Structure

---
+++ src/main/java/com/project/Ecommerce/entity/User.java
@@ -0,0 +1,26 @@
+package com.project.Ecommerce.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Entity
+@Document
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String address;
+    private String number;
+}
---
+++ src/main/java/com/project/Ecommerce/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.project.Ecommerce.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}
